5a65615d88229bee87d3f1624e10c1c26e34501a
nadiaperesoficial-hash <nadiaperesoficial@gmail.com>
2026-07-04 04:16:59 -0300

    Create MainActivity.java

---
+++ android/app/src/main/java/com/whisper/app/MainActivity.java
@@ -0,0 +1,9 @@
+package com.whisper.app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+    // No padrão moderno de Flutter (v2 embedding),
+    // a classe base FlutterActivity já gerencia automaticamente
+    // o ciclo de vida e o registro de plugins.
+}
