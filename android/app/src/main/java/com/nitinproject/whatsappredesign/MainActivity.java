d7d0120fa785d7ab4730755b90a09b1667fdfacd
nadiaperesoficial-hash <nadiaperesoficial@gmail.com>
2026-07-04 04:17:52 -0300

    Delete android/app/src/main/java/com/nitinproject/whatsappredesign directory

--- android/app/src/main/java/com/nitinproject/whatsappredesign/MainActivity.java
+++
@@ -1,9 +0,0 @@
-package com.nitinproject.whatsappredesign;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-    // No padrão moderno de Flutter (v2 embedding), 
-    // a classe base FlutterActivity já gerencia automaticamente 
-    // o ciclo de vida e o registro de plugins.
-}
