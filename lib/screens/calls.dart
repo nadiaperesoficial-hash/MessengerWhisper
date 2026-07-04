471e2acab2f3a0e6cf9ef5c3f92778ce6ea08508
nadiaperesoficial-hash <nadiaperesoficial@gmail.com>
2026-07-04 14:35:44 -0300

    Update calls.dart

--- lib/screens/calls.dart
+++ lib/screens/calls.dart
@@ -1,99 +1,103 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp_redesign/model/calls_model.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import '../core/theme/app_theme.dart';
 
 class Calls extends StatelessWidget {
-  Calls(this.listType);
+  Calls(this.listType, {super.key});
   final String listType;
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-        backgroundColor: Colors.grey[200],
-        appBar: new AppBar(
-          title: new Text(
-            listType,
-            style: new TextStyle(color: const Color(0xFFFFFFFF)),
-          ),
-          titleSpacing: -1.0,
-          leading:
-              new IconButton(icon: const Icon(Icons.search), onPressed: () {}),
-          actions: <Widget>[
-            new IconButton(icon: const Icon(Icons.more_vert), onPressed: () {}),
-          ],
-        ),
-        body: new ListView.builder(
-            itemCount: callMockData.length,
-            itemBuilder: (context, position) => new Padding(
-                padding: const EdgeInsets.all(0.0),
-                child: new Card(
-                  elevation: 1.0,
-                  color: Colors.white,
-                  child: new Column(
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(listType),
+        titleSpacing: -1.0,
+        leading: IconButton(icon: const Icon(Icons.search), onPressed: () {}),
+        actions: <Widget>[
+          IconButton(icon: const Icon(Icons.more_vert), onPressed: () {}),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: callMockData.length,
+        itemBuilder: (context, position) => Padding(
+          padding: const EdgeInsets.all(0.0),
+          child: Card(
+            elevation: 1.0,
+            color: Colors.white,
+            child: Column(
+              children: <Widget>[
+                ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: CachedNetworkImageProvider(
+                        callMockData[position].profileImageUrl),
+                    backgroundColor: Colors.grey,
+                  ),
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
-                      new ListTile(
-                        leading: new CircleAvatar(
-                          backgroundImage: new CachedNetworkImageProvider(
-                              callMockData[position].profileImageUrl),
-                          backgroundColor: Colors.grey,
-                        ),
-                        title: new Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            new Text(
-                              callMockData[position].name,
-                              style: new TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black),
-                            ),
-                            new Icon(
-                                callMockData[position].callSource == 'video'
-                                    ? Icons.videocam
-                                    : Icons.call,
-                                color: const Color(0xFF2845E7))
-                          ],
-                        ),
-                        subtitle: new Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            new Column(
-                              children: <Widget>[
-                                new Row(
-                                 
-                                  children: <Widget>[
-                                    //day of call
-                                    new Text(
-                                      callMockData[position].day,
-                                      style: new TextStyle(
-                                          fontWeight: FontWeight.normal,
-                                          color: Colors.grey),
-                                    ),
-                                    new Text(' | ', style: new TextStyle(fontWeight: FontWeight.bold,
-                                          color: Colors.grey),),
-                                    
-                                    //time of call
-                                    new Text(
-                                      callMockData[position].time,
-                                      style: new TextStyle(
-                                          fontWeight: FontWeight.normal,
-                                          color: Colors.grey),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                            new Column(children: <Widget>[ new Text(
-                                      callMockData[position].callType,
-                                      style: new TextStyle(
-                                          fontWeight: FontWeight.normal,
-                                          color: Colors.grey),
-                                    ),],),
-                          ],
-                        ),
+                      Text(
+                        callMockData[position].name,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
+                      ),
+                      Icon(
+                        callMockData[position].callSource == 'video'
+                            ? Icons.videocam
+                            : Icons.call,
+                        color: AppColors.lineGreen,
                       ),
                     ],
                   ),
-                ))));
+                  subtitle: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              Text(
+                                callMockData[position].day,
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.normal,
+                                    color: Colors.grey),
+                              ),
+                              const Text(
+                                ' | ',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.grey),
+                              ),
+                              Text(
+                                callMockData[position].time,
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.normal,
+                                    color: Colors.grey),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Text(
+                            callMockData[position].callType,
+                            style: const TextStyle(
+                                fontWeight: FontWeight.normal,
+                                color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
