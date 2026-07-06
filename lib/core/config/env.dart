c02371c7486407d3a67580666acdb149c8c533c2
nadiaperesoficial-hash <nadiaperesoficial@gmail.com>
2026-07-06 20:19:01 -0300

    Update env.dart

--- lib/core/config/env.dart
+++ lib/core/config/env.dart
@@ -1,7 +1,8 @@
-// lib/core/config/env.dart
 class Env {
   static const supabaseUrl = String.fromEnvironment('SUPABASE_URL');
   static const supabaseAnonKey = String.fromEnvironment('SUPABASE_ANON_KEY');
+  static const supabaseBUrl = String.fromEnvironment('SUPABASE_B_URL');
+  static const supabaseBAnonKey = String.fromEnvironment('SUPABASE_B_ANON_KEY');
   static const agoraAppId = String.fromEnvironment('AGORA_APP_ID');
   static const giphyApiKey = String.fromEnvironment('GIPHY_API_KEY');
 
